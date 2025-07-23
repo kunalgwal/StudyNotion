42c6a0d6332b77858167b6208cc639c5df7a7754
kunalgwal <kunalgwal1108@gmail.com>
2025-07-23 17:00:39 +0530

    Initial commit

---
+++ server/config/database.js
@@ -0,0 +1,19 @@
+//initiating mongoose
+const mongoose = require("mongoose");
+//importing from .env file
+require("dotenv").config();
+
+const { MONGODB_URL } = process.env;
+//connecting with database
+exports.connect = () => {
+    mongoose.connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    } )
+};
