9c08b7a3e5f16dfdb4be04ec8c73d9ef2a4d182b
kunalgwal <144695546+kunalgwal@users.noreply.github.com>
2025-07-23 18:08:43 +0530

    Delete server directory

--- server/routes/Contact.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { contactUsController } = require("../controllers/contactUs")
-
-router.post("/contact", contactUsController)
-
-module.exports = router
